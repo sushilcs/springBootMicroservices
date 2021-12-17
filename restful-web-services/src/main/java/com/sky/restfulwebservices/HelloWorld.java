543e575c2aeb23f6e484cbfc0b24df2ff1137419
sushilcs <sushilkr14dec@ggmail.com>
2021-12-18 04:14:59 +0530

    constructor injection with autowiring

--- restful-web-services/src/main/java/com/sky/restfulwebservices/HelloWorld.java
+++ restful-web-services/src/main/java/com/sky/restfulwebservices/HelloWorld.java
@@ -1,15 +1,26 @@
 package com.sky.restfulwebservices;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorld {
+	@Autowired
+	@Value(value = "hello worldssssssss")
+	HelloWorldBean bean;
 	
 	//method returning hello world;
 	@GetMapping("/helloworld")
 	public String ssayHello() {
 		return "helloworld";
 	}
+	
+	//method returning hello world;
+		@GetMapping("/helloworld-bean")
+		public String ssayHelloBean() {
+			return bean.getMessage();
+		}
 
 }
